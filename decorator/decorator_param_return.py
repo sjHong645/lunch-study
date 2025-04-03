e973bb437f2c9639916e167bcd886694a4e6ab5c
sjHong645 <ixdos1111@gmail.com>
2025-04-03 12:10:14 +0900

    매개변수와 반환값이 있는 데코레이터 만들기

---
+++ decorator/decorator_param_return.py
@@ -0,0 +1,14 @@
+
+def trace(func) :               # 호출할 함수를 매개변수로 받음
+    def wrapper(a, b) :         # 호출할 함수 add(a, b)의 매개변수와 똑같이 지정
+        r = func(a, b)          # func에 매개변수 a, b를 넣어서 호출하고 반환값을 변수에 저장
+        print('{0}(a={1}, b={2}) -> {3}'.format(func.__name__, a, b, r))  # 매개변수와 반환값 출력
+        
+        return r               # func의 반환값을 반환
+    return wrapper             # wrapper 함수 반환
+
+@trace
+def add(a, b) :
+    return a + b
+
+print(add(10, 20))
